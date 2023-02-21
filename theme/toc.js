9f3bb9864563f80ff2fa42f65997186a9b12780d
Sugar-Coder <13169670256@163.com>
2023-02-21 20:38:46 +0800

    add TOC for blog

---
+++ theme/toc.js
@@ -0,0 +1,74 @@
+var initToc = function () {
+    // var path = window.location.pathname;
+    // // do not load TOC at home
+    // if (path == '/') {
+    //     return;
+    // }
+
+    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+        el.addEventHandler("click", function () {
+            Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+                el.classList.remove("active");
+            });
+            el.classList.add("active");
+        });
+    });
+
+    var updateFunction = function () {
+        var id = null;
+        var elements = document.getElementsByClassName("header");
+        Array.prototype.forEach.call(elements, function (el) {
+            if (window.pageYOffset >= el.offsetTop) {
+                id = el;
+            }
+        });
+
+        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+            el.classList.remove("active");
+        });
+
+        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function (el) {
+            if (id == null) {
+                return;
+            }
+            if (id.href.localeCompare(el.href) == 0) {
+                el.classList.add("active");
+            }
+        });
+    };
+
+    var pagetoc = document.getElementsByClassName("pagetoc")[0];
+    var elements = document.getElementsByClassName("header");
+    Array.prototype.forEach.call(elements, function (el) {
+        var link = document.createElement("a");
+
+        // Indent shows hierarchy
+        var indent = "";
+        switch (el.parentElement.tagName) {
+            case "H1":
+                return;
+            case "H2":
+                indent = "10px";
+                break;
+            case "H3":
+                indent = "20px";
+                break;
+            case "H4":
+                indent = "30px";
+                break;
+            default:
+                break;
+        }
+
+        link.appendChild(document.createTextNode(el.text));
+        link.style.paddingLeft = indent;
+        link.href = el.href;
+        pagetoc.appendChild(link);
+    });
+    updateFunction.call();
+
+    // Handle active elements on scroll
+    window.addEventListener("scroll", updateFunction);
+}
+
+window.addEventListener('load', initToc);
