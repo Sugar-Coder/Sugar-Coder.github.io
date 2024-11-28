b5ab45e638394abe99240f4f418f35b0b6679a99
Deploy from CI <>
2024-11-28 14:45:32 +0000

    Deploy a2698328a2230ef5fe15d20d4e89e6aac9dc445d to gh-pages

---
+++ theme/comments.js
@@ -0,0 +1,47 @@
+'use strict';
+
+function loadComments() {
+    // console.log("loading comments.");
+    const page = document.getElementById("comment-container");
+
+    const isLight = document.querySelector('html').getAttribute('class').indexOf('light') != -1;
+
+    const commentScript = document.createElement('script')
+    const commentsTheme = isLight ? 'github-light' : 'github-dark'
+    commentScript.async = true
+    commentScript.src = 'https://utteranc.es/client.js'
+    commentScript.setAttribute('repo', 'Sugar-Coder/Sugar-Coder.github.io')
+    commentScript.setAttribute('issue-term', 'pathname')
+    commentScript.setAttribute('id', 'utterances')
+    commentScript.setAttribute('label', 'comment')
+    commentScript.setAttribute('theme', commentsTheme)
+    commentScript.setAttribute('crossorigin', 'anonymous')
+
+    page.appendChild(commentScript);
+}
+
+function removeComments() {
+    const page = document.getElementById("comment-container");
+    page.removeChild(page.firstChild);
+}
+
+loadComments();
+
+(function observeChange() {
+    const html=document.querySelector('html')
+    const options={
+        attributes:true,//观察node对象的属性
+        attributeFilter:['class']//只观察class属性
+    }
+    let prevIsLight = document.querySelector('html').getAttribute('class').indexOf('light') != -1;
+    var mb=new MutationObserver(function(mutationRecord,observer){
+        let isLight = document.querySelector('html').getAttribute('class').indexOf('light') != -1;
+        // console.log(`prevIsLight:${prevIsLight}, isLight:${isLight}`)
+        if (prevIsLight != isLight) {
+            removeComments();
+            loadComments();
+            prevIsLight = isLight;
+        }
+    })
+    mb.observe(html,options)
+})();
